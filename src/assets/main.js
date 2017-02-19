0d65cc50cd264b92133463940c7d6588e9ff4f95
Kushal Dharpur <M1039339@mindtree.com>
2017-02-19 15:47:31 +0530

    function19

--- src/assets/main.js
+++ src/assets/main.js
@@ -22,7 +22,7 @@ function guess() {
         showAnswer(false);
         showReplay();
     } else {
-        setMessage('"Incorrect, try again."');
+        setMessage('Incorrect, try again.');
     }
 
 }
