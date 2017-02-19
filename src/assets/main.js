181e46a2c01cec1d727b5b923fd03225834b0979
Kushal Dharpur <M1039339@mindtree.com>
2017-02-19 13:58:20 +0530

    function12

--- src/assets/main.js
+++ src/assets/main.js
@@ -4,6 +4,9 @@ let attempt = document.getElementById('attempt');
 function guess() {
     let input = document.getElementById('user-guess');
     //add functionality to guess function here
+    if (answer.value == '' || attempt.value == '') {
+        setHiddenFields();
+    }
     if (!validateInput(input.value)) {
         return false;
     } else {
@@ -33,4 +36,19 @@ function validateInput(l) {
         setMessage("Guesses must be exactly 4 characters long.");
         return false;
     }
+}
+
+function getResults(input) {
+    let htmltag = '<div class="row"><span class="col-md-6">' + input + '</span><div class="col-md-6">';
+    for (i = 0; i < input.value.length < 4; i++) {
+        if (input.charAt(i) == answer.value.charAt(i)) {
+            htmltag += '<span class="glyphicon glyphicon-ok"></span>';
+        } else if (input.value > ) {
+            htmltag += '<span class="glyphicon glyphicon-transfer"></span>';
+        } else {
+            htmltag += '<span class="glyphicon glyphicon-remove"></span>';
+        }
+    }
+    htmltag += '</div></div>';
+
 }
